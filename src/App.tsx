b90c43382a02d134fd2d3015b4e7f82d0182512b
Valera <valerakaktotak@gmail.com>
2024-07-14 15:18:31 +0300

    done Login Page and some fixes in context and protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useContext, type FC } from 'react'
+import { type FC } from 'react'
 import { RouterProvider } from 'react-router-dom'
 
 //Routes
 import { router } from '@/routes'
-import { userAuthContext } from './context/UserAuthContext'
 import { UserAuthProvider } from './context/UserAuthProvider'
 
 const App: FC = () => {
-  const { user } = useContext(userAuthContext)
-
-  console.log(user)
   return (
     <UserAuthProvider>
       <RouterProvider router={router} />
