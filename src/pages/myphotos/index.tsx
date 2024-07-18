6fc9e8591eb09d6a77aac45eba6338cda1fec535
Valera <valerakaktotak@gmail.com>
2024-07-18 20:04:57 +0300

    done My photos Page

--- src/pages/myphotos/index.tsx
+++ src/pages/myphotos/index.tsx
@@ -1,3 +1,4 @@
+import { HeartIcon } from 'lucide-react'
 import { useContext, useEffect, useState, type FC } from 'react'
 
 //Context
@@ -15,6 +16,7 @@ import Layout from '@/components/layout'
 const MyPhotos: FC = () => {
   const { user } = useContext(userAuthContext)
   const [userData, setUserData] = useState<DocumentResponse[]>([])
+  console.log(userData)
 
   const getAllPost = async (id: string) => {
     try {
@@ -46,9 +48,41 @@ const MyPhotos: FC = () => {
     }
   }, [])
 
+  const renderPost = () => {
+    return userData.map((item) => {
+      return (
+        <div key={item.photos[0].uuid} className='relative'>
+          <div className='group absolute bottom-0 left-0 right-0 top-0 h-full w-full bg-transparent transition-all duration-200 hover:bg-slate-950 hover:bg-opacity-75'>
+            <div className='flex h-full w-full flex-col items-center justify-center'>
+              <HeartIcon className='hidden fill-white group-hover:block' />
+              <div className='hidden text-white group-hover:block'>
+                {item.likes} likes
+              </div>
+            </div>
+          </div>
+          <img
+            src={`${item.photos[0].cdnUrl}/-/progressive/yes/-/scale_crop/300x300/center/`}
+          />
+          <p>{item.caption}</p>
+        </div>
+      )
+    })
+  }
+
   return (
     <Layout>
-      <div>MyPhotos</div>
+      <div className='flex justify-center'>
+        <div className='w-full max-w-3xl border'>
+          <h3 className='bg-slate-800 p-2 text-center text-lg text-white'>
+            My Photos
+          </h3>
+          <div className='p-8'>
+            <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
+              {userData ? renderPost() : <div>...Loading</div>}
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
